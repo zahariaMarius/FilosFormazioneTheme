30eb9101623a58c19a9c01904b0348ce468f989e
Zaharaia Laurentiu Jr Marius <zaharialorenzo@gmail.com>
2018-01-24 22:45:18 +0100

    tolti menu inutili

--- inc/menu.php
+++ inc/menu.php
@@ -4,9 +4,6 @@
 function register_custom_menus() {
   register_nav_menus( array(
       'header' 	=> 	'Header Menu',
-      'footer1' => 'Footer Left',
-      'footer2' => 'Footer Center',
-      'footer3' => 'Footer Right',
   ));
 }
 add_action( 'init', 'register_custom_menus' );
@@ -41,75 +38,5 @@ function header_menu() {
       )
   );
 }
-
-function footer_menu_1() {
-  wp_nav_menu(
-      array(
-          'theme_location'  => 'footer1',
-          'menu'            => '',
-          'container'       => '',
-          'container_class' => '',
-          'container_id'    => '',
-          'menu_class'      => '',
-          'menu_id'         => '',
-          'echo'            => true,
-          'fallback_cb'     => '',
-          'before'          => '',
-          'after'           => '',
-          'link_before'     => '',
-          'link_after'      => '',
-          'items_wrap'      => '<ul class="footer-menu-1">%3$s</ul>',
-          'depth'           => 2,
-          'walker'          => ''
-      )
-  );
-}
-
-function footer_menu_2() {
-  wp_nav_menu(
-      array(
-          'theme_location'  => 'footer2',
-          'menu'            => '',
-          'container'       => '',
-          'container_class' => '',
-          'container_id'    => '',
-          'menu_class'      => '',
-          'menu_id'         => '',
-          'echo'            => true,
-          'fallback_cb'     => '',
-          'before'          => '',
-          'after'           => '',
-          'link_before'     => '',
-          'link_after'      => '',
-          'items_wrap'      => '<ul class="footer-menu-2">%3$s</ul>',
-          'depth'           => 2,
-          'walker'          => ''
-      )
-  );
-}
-
-function footer_menu_3() {
-  wp_nav_menu(
-      array(
-          'theme_location'  => 'footer3',
-          'menu'            => '',
-          'container'       => '',
-          'container_class' => '',
-          'container_id'    => '',
-          'menu_class'      => '',
-          'menu_id'         => '',
-          'echo'            => true,
-          'fallback_cb'     => '',
-          'before'          => '',
-          'after'           => '',
-          'link_before'     => '',
-          'link_after'      => '',
-          'items_wrap'      => '<ul class="footer-menu-3">%3$s</ul>',
-          'depth'           => 2,
-          'walker'          => ''
-      )
-  );
-}
-
 
 ?>
