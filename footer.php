83df6ebc9c247f5d1398b43345ad1804ae764eec
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-01-31 12:43:35 +0100

    mok commit

--- footer.php
+++ footer.php
@@ -1,26 +1,24 @@
   <!-- Chiudo page container -->
  <!-- </div>  -->
+ 
 
 
-  <!-- <div class="footer">
-    <div class="footer-wrapper">
-      <div class="footer-section">
-        <div class="footer-section-title">Link Utili</div>
-        <?php // footer_menu_1(); ?>
-      </div>
-      <div class="footer-section">
-        <div class="footer-section-title">Area Personale</div>
-        <?php // footer_menu_2(); ?>
-      </div>
-      <div class="footer-section">
-        <div class="footer-section-title">Legale</div>
-        <?php // footer_menu_3(); ?>
-      </div>
-      <div class="footer-section">
-        <img class="footer-logo" src=" <?php //echo get_template_directory_uri(); ?>/imgs/logo.png" alt="">
-      </div>
-    </div>
-  </div> -->
+  <div class="container-fluid">
+    <div class="row">
+    <div class="footer">
+    <div class="card bg-dark text-white" style= "width: 100%; height: 100%;">
+    <div class="card-img-overlay">
+    <img class="card-img" id="card-img-footer" src="<?php echo get_template_directory_uri(); ?>/imgs/footer_img.jpg" alt="Card image">
+ 
+   
+  </div>
+</div>
+</div>
+    
+    
+  </div>
+</div>
+ 
 
  <!-- Questa funzione di Wordpress carica tutti i tag del footer impostati da Wordpress -->
   <?php wp_footer(); ?>
