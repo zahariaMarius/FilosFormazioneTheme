85c0cf5969e1869ef7350b6f97009baecdcb2670
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-02-02 20:48:16 +0100

    servizio lavoro commit

--- servizioLavoro.php
+++ servizioLavoro.php
@@ -1,7 +1,52 @@
 <?php /* Template Name: Servizio lavoro */ ?>
 
 <?php get_header(); ?>
-
-SERVIZIO LAVORO
-
+<section class="servizi-section">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <h1 class="hServizi"> Servizio al lavoro</h1>
+            </div>
+        </div>
+            <div class="row justify-content-center body" style="background: url(<?php echo get_template_directory_uri(); ?>/imgs/servizi-lavoro.png); background-size: 100% 100%;">
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col-12 body-content">
+                            <p class="Title">1. INFORMAZIONE</p>
+                            <p class="Text">Sostenere l'utente nell'acquisire informazioni utili nell'orientarsi e
+                            promuoversi nel mercato del lavoro e presentare i servizi offerti dalla rete regionale pubblico/privata.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 body-content">
+                            <p class="Title">2. ACCOGLIENZA</p>
+                            <p class="Text">
+                                Garantire all'utente la possibilità di richiesta e rilascio della Dichiarazione di immediata disponibilità(DID) la cui
+                                certificazione è di competenza della segreteria.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 body-content">
+                            <p class="Title">3. ORIENTAMENTO PROFESSIONALE</p>
+                            <p class="Text">
+                                Sostenere l'utente nella costruzione e definizione di un percorso personalizzato utile a promuovere attivamente il suo inserimento
+                                o il reinserimento nel mondo del lavoro o a migliorare la posizione nel mercato del lavoro.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 body-content">
+                            <p class="Title">4. CONSULENZA ORIENTATIVA</p>
+                            <p class="Text">
+                               Sostenere l'utente che necessita di servizi di supporto per l'individuazione dell'obiettivo professionale, al fine di definire il percorso
+                               utile a promuoverne attivamente l'inserimento o il reinserimento o a migliorare la posizione nel mercato.
+                            </p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
