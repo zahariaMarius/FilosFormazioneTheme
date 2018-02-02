dd8d5c73b70fcc193274dc304b8ac7ecac73af56
Zaharaia Laurentiu Jr Marius <zaharialorenzo@gmail.com>
2018-02-02 21:33:51 +0100

    add comments for backend

--- homepage.php
+++ homepage.php
@@ -1,3 +1,13 @@
+<!--  
+    Ogni blocco html è stato commentato in modo da capire dove inserire funzioni php che
+    recuparano i dati dal DB.
+    Rendere la pagina dinamica andando a sostituire i dati fittizzi.
+    Creare ogni elemento avendo il suo scheletro e seguire i commenti per capire dove
+    inserire e quale dato inserire al suo interno.
+    Una volta completato un elemento eliminare gli altri inutili, come in esempio
+    nella carousel-section.
+-->
+
 <?php /* Template Name: Homepage */ ?>
 <?php get_header(); ?>
 
@@ -11,6 +21,7 @@
                 <li data-target="#newsCarousel" data-slide-to="2"></li>
                 <!-- inserire il contenuto della data-slide-to per ogni indice presente - start -->
             </ol>
+            <!-- per ogni news creare un carousel-item -->
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <!-- inserire il contenuto della slideshow in questo blocco solamente l'immagine - start -->
@@ -18,12 +29,13 @@
                     <!-- inserire il contenuto della slideshow in questo blocco, solamente l'immagine - end -->
                     <div class="carousel-caption d-none d-md-block">
                         <!-- inserire il contenuto della slideshow in questo blocco, ripettivamente: Titolo, Sottotitolo e bottone - start -->
-                        <h1 class="carousel-title display-1">Open Day</h1> 
+                        <h1 class="carousel-title display-1">Open Day</h1>
                         <h5 class="carousel-subtitle display-4">Vieni a conoscre i nuovi corsi 2018</h5>
                         <a class="carousel-button btn btn-danger text-center" href="#">Scopri di più</a>
                         <!-- inserire il contenuto della slideshow in questo blocco, ripettivamente: Titolo, Sottotitolo e bottone - end -->
                     </div>
                 </div>
+                <!-- una volta ciclato il primo eliminare questi - start -->
                 <div class="carousel-item">
                     <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/imgs/carousel/slideimage.jpeg" alt="Second slide">
                     <div class="carousel-caption d-none d-md-block">
@@ -41,6 +53,8 @@
                     </div>
                 </div>
             </div>
+            <!-- una volta ciclato il primo eliminare questi - end -->    
+
             <a class="carousel-control-prev" href="#newsCarousel" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
@@ -53,11 +67,12 @@
     </section>
     <!-- section that contain the slideshow - end -->
 
-    
+
     <section class="courses_section mt-5">
         <div class="container-fluid">
             <div class="row justify-content-center">
                 <div class="col-sm-2 text-center">
+                    <!-- inserire il nome della sezione -->
                     <h1 class="display-3 section-title">Corsi</h1>
                 </div>
             </div>
@@ -65,6 +80,7 @@
             <!-- qua i contenuti sono pari quindi le righe che contengono 2 macro corsi a testa
                 sotto inizia la riga con classe .row, importante mantenere le stesse classi -->
             <div class="row justify-content-center">
+                <!-- per ogni corso craere una col -->
                 <div class="col-sm-4 p-5">
                     <div class="card card-corso rounded-0 text-center">
                         <!-- inserire il contenuto dei macro corsi offerti in questo blocco, rispettivamente: immagine di sfondo, icona che rappresenta il corso e nome del corso - start -->
@@ -73,9 +89,9 @@
                             <img class="card-title" src="<?php echo get_template_directory_uri(); ?>/imgs/corsi/iconaMeccanica.png" alt="">
                             <p class="card-text">MECCANICA</p>
                         </div>
-                        <!-- inserire il contenuto dei macro corsi offerti in questo blocco, rispettivamente: immagine di sfondo, icona che rappresenta il corso e nome del corso - start -->                        
+                        <!-- inserire il contenuto dei macro corsi offerti in questo blocco, rispettivamente: immagine di sfondo, icona che rappresenta il corso e nome del corso - start -->
                     </div>
-                </div> 
+                </div>
                 <div class="col-sm-4 p-5">
                     <div class="card card-corso rounded-0 text-center">
                         <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/imgs/corsi/logoTermoidraulica.jpeg" alt="logo corso termoidraulica">
@@ -114,10 +130,11 @@
     <!-- section trova corso -start -->
     <section class="trova-corso-section mt-5" style="background: url(<?php echo get_template_directory_uri(); ?>/imgs/sectionTrovacorso.jpeg) no-repeat; background-size: 100% 100%;">
         <div class="container-fluid">
-        
+
         <div class="form-container">
             <div class="row justify-content-center mb-5">
                 <div class="col-sm-6 text-center">
+                    <!-- inserire il nome della sezione -->
                     <h1 class="display-3">Trova il tuo corso</h1>
                 </div>
             </div>
@@ -166,10 +183,13 @@
         <div class="container-fluid">
         <div class="row justify-content-center">
                 <div class="col-sm-6 text-center">
+                    <!-- inserire il nome della sezione -->
                     <h1 class="display-3 section-title">News</h1>
                 </div>
             </div>
+            <!-- Ogni tre news creare una row -->
             <div class="row justify-content-center">
+                <!-- creare una colonna per ogni news -->
                 <div class="col-sm-4 p-5">
                     <div class="card">
                     <!-- per ogni news inserire l'immagine qua sotto, con alt rispettivo -->
@@ -273,6 +293,7 @@
 
 
     <!-- section filos in breve - start -->
+    <!-- contenuto statico -->
     <section class="section-filos-in-breve">
         <div class="container-fluid">
             <div class="row justify-content-center">
@@ -294,6 +315,7 @@
     </section>
     <!-- section filos in breve - end -->
 
+
      <!-- section sedi -start -->
     <section class="sedi-section" style="background: url(<?php echo get_template_directory_uri(); ?>/imgs/sedeBackground.png) no-repeat center center; background-size: 35% 100%;">
         <div class="container-fluid">
@@ -303,8 +325,10 @@
             <div class="row justify-content-center card-row">
                 <div class="col-sm-4 p-5 card-col">
                 <div class="card">
+                    <!-- inserire di seguito l'immagine delle sede -->
                     <img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/imgs/news/news6.jpeg" alt="">
                         <div class="sede">
+                            <!-- inserire di seguito il nome della sede -->
                             <p>Novara</p>
                         </div>
                     </div>
