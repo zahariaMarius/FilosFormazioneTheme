c554e57c157064907d3566028825940f06e6596c
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-02-02 14:08:27 +0100

    chi siamo quasi fatto

--- chiSiamo.php
+++ chiSiamo.php
@@ -4,10 +4,70 @@
 
 <section class="chisiamo-section">
     <h1 class="hChisiamo">Chi Siamo</h1>
-    <div class="row">
+    <div class="row ">
         <div class="col-12">
             <img class="chi-siamo-img"src="<?php echo get_template_directory_uri(); ?>/imgs/chi-siamo-img.png" alt="">
         </div>
+    </div>
+    <div class="row justify-content-center">
+            <div class="col-2">
+                 <img class="icona-left"src="<?php echo get_template_directory_uri(); ?>/imgs/Icona-1.png" alt="">
+            </div>
+            <div class="col-8">
+                <p class="p-left">Le attività che FILOS Formazione mette in campo si ispirano come per il passato alla centralità della persona e all'inclusione sociale attraverso la formazione e si fanno
+                interpreti della domanda di professionalità proveniente dal mondo produttivo.
+                Dunque grande cura alle esigenze delle persone in cerca di lavoro, ma anche una forte attenzione ai processi di innovazione presenti all'interno dell'impresa e al ruolo
+                che la formazione può giocare nell'aggiornamento professionale dei lavoratori occupati.
+                </p>
+            </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <p class="p-right">
+                Momento centrale dell'impegno di FILOS FORMAZIONE è rappresentato dai percorsi di formazione iniziale di Obbligo di Istruzione.
+                Oltre 180 ragazze e ragazzi seguono un percorso che li porterà ad acquisire una qualifica professionale nel settore dei servizi estetici, della termoidraulica e della meccanica:
+                sono allievi che spesso provengono da esperienza scolastiche difficili e che richiedono da parte dei docenti e della struttura un impegno particolare.
+            </p>
+        </div>
+        <div class="col-2">
+        <img class="icona-right"src="<?php echo get_template_directory_uri(); ?>/imgs/Icona-2.png" alt="">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+         <div class="col-2">
+         <img class="icona-left"src="<?php echo get_template_directory_uri(); ?>/imgs/Icona-3.png" alt="">
+         </div>
+         <div class="col-8">
+             <p class="p-left">
+                 Per quanto riguarda le opportunità rivolte agli adulti FILOS FORMAZIONE raccoglie il testimone della grande esperienza maturata nell'ultimo decennio da IAL 
+                 relativamente alle attività formative nel settore socio-assistenziale rivolte sia a disoccupati che a occupati.
+                 E' questo un settore che rappresenta un effettivo sbocco professionale data la domanda proveniente dal sistema dell'assistenza alla persona.
+                 L'agenzia è diventata punto di riferimento per la prima formazione di Operatori socio-sanitari e per l'aggiornamento e la riqualificazione del personale occupato presso le strutture del territorio.
+            </p>
+         </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <p class="p-right">
+               FILOS FORMAZIONE, inoltre, mette a dispozione degli occupati e di coloro che intendono migliorare le proprie competenze corsi serali di lingue, informatica, servizi estetici, etc.
+               La storia dell'agenzia formativa di via Negri è ricca di esperienze nel campo dell'inclusione sociale, in particolare rivolte alle categorie deboli del mercato del lavoro.
+               Questo lavoro è stato svolto entrando in rapporto e costruendo una "rete" con i principali attori che a livello territoriale si occupano di questo problema.
+            </p>
+        </div>
+        <div class="col-2">
+        <img class="icona-right"src="<?php echo get_template_directory_uri(); ?>/imgs/Icona-4.png" alt="">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+         <div class="col-2">
+         <img class="icona-left"src="<?php echo get_template_directory_uri(); ?>/imgs/Icona-5.png" alt="">
+         </div>
+         <div class="col-8">
+             <p class="p-left">
+                Da non trascurare le occasioni formative offerte a tutti coloro(giovani, adulti e disoccupati) che cercano di acquisire o incrementare competenze nei settori direttamente legati al mondo del terziario e della produzione.
+                FILOS FORMAZIONE dispone di laboratori attrezzati e ha una grande tradizione di formazione nel settore della saldatura, macchine a controllo numerico e termoidraulico, senza dimenticare le nuove tecnologie del solare termico.
+            </p>
+         </div>
     </div>
 
 </section>
