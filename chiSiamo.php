35208929ef3a9a8e07d266239fd21e601152c1d0
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-02-02 11:02:13 +0100

    chi siamo e footer

--- chiSiamo.php
+++ chiSiamo.php
@@ -2,6 +2,14 @@
 
 <?php get_header(); ?>
 
-CHI SIAMO
+<section class="chisiamo-section">
+    <h1 class="hChisiamo">Chi Siamo</h1>
+    <div class="row">
+        <div class="col-12">
+            <img class="chi-siamo-img"src="<?php echo get_template_directory_uri(); ?>/imgs/chi-siamo-img.png" alt="">
+        </div>
+    </div>
+
+</section>
 
 <?php get_footer(); ?>
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 /*
 jQuery è già installato da wordpress, quindi non serve aggiungerlo.
@@ -8,21 +7,3 @@ quindi per prima cosa si può assegnarlo alla variabile $ se siete abituati ad u
 window.$ = window.jQuery;
 
 
-=======
-
-/*
-jQuery è già installato da wordpress, quindi non serve aggiungerlo.
-Unica nota è che non lo troverete assegnato alla variabile di window `$`, bensì alla variabile `jQuery`,
-quindi per prima cosa si può assegnarlo alla variabile $ se siete abituati ad utilizzarlo così.
-*/
-window.$ = window.jQuery;
-$(".card mb-3").hover(function () {
-    $(".card-img-top").stop().animate({
-        opacity: 1.0
-        }, "slow");
-    $(".bordo").fadeIn("slow");
-});
-
-$(".card mb-3").addClass("color");
-
->>>>>>> e0b5e6e685acbc37c062f2dc2b837091ef01abdc
